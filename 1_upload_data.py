7a1f773008159d938132f1e3d52c3faf510f855d
Sheldon <sheldonsebastian94@gmail.com>
2024-10-04 20:00:27 -0400

    Upload data to Azure ML workspace

--- 1_upload_data.py
+++ 1_upload_data.py
@@ -1,19 +1,23 @@
-# Import necessary libraries
+# https://learn.microsoft.com/en-us/azure/machine-learning/tutorial-explore-data?view=azureml-api-2
+"""Upload data to Azure ML workspace."""
+
+# %%
 from azure.ai.ml import MLClient
 from azure.identity import DefaultAzureCredential
 from azure.ai.ml.entities import Data
 from azure.ai.ml.constants import AssetTypes
-from azure.ai.ml.operations import DatastoreOperations
 from dotenv import load_dotenv
 import os
 
-# Load environment variables
+# %%
 if os.path.exists("configs.env"):
     load_dotenv("configs.env")
 
-# Authenticate with Azure
+# %%
+# authenticate
 credential = DefaultAzureCredential()
 
+# %%
 # Get a handle to the workspace
 ml_client = MLClient(
     credential=credential,
@@ -22,13 +26,14 @@ ml_client = MLClient(
     workspace_name=os.getenv("WORKSPACE_NAME"),
 )
 
-# Define the path to the local data
+# %%
 data_path = "data"
 
-# Set the version number of the data asset
+# %%
+# set the version number of the data asset
 v1 = "initial"
 
-# Define the data asset
+# %%
 my_data = Data(
     name="fashion-mnist",
     version=v1,
@@ -37,25 +42,20 @@ my_data = Data(
     type=AssetTypes.URI_FOLDER,
 )
 
-# Create the data asset if it doesn't exist
+# %%
+## create data asset if it doesn't already exist:
 try:
     data_asset = ml_client.data.get(name="fashion-mnist", version=v1)
     print(
         f"Data asset already exists. Name: {my_data.name}, version: {my_data.version}"
     )
-except Exception as e:
+except:
     ml_client.data.create_or_update(my_data)
     print(f"Data asset created. Name: {my_data.name}, version: {my_data.version}")
 
-# Retrieve the data asset
+# %%
+# consuming the data
 data_asset = ml_client.data.get(name="fashion-mnist", version=v1)
 print(f"Data asset: {data_asset.name}, version: {data_asset.version}")
 
-# Download the data asset using the MLClient's data download method
-local_directory = "downloaded_data"
-os.makedirs(local_directory, exist_ok=True)
-
-# Download the asset to the specified local directory
-ml_client.data.download(name="fashion-mnist", version=v1, download_path=local_directory)
-
-print(f"Data downloaded to {local_directory}")
+# %%
